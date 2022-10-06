10009c62c9af807c1f4fa3bd75b03b2868f83c1d
Tomasz Poniatowski <tomasz.poniatowski@cytawa.com>
2022-10-06 19:15:42 +0200

    -Begining-

---
+++ src/main/java/com/example/split/SplitApplication.java
@@ -0,0 +1,13 @@
+package com.example.split;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitApplication.class, args);
+    }
+
+}
